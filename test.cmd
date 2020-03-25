f5d924b0da225db9a2d27afadf90021566892b6a
till straumann <till.straumann@alumni.tu-berlin.de>
2020-03-25 11:59:55 +0100

    test.cmd: added test script

---
+++ test.cmd
@@ -0,0 +1,25 @@
+help c10
+help c9
+help c8
+help c7
+help c6
+help c5
+help c4
+help c3
+help c2
+help c1
+help c0
+c0  0 1 2 3 4 5 6 7 8 9
+c1  0 1 2 3 4 5 6 7 8 9
+c2  0 1 2 3 4 5 6 7 8 9
+c3  0 1 2 3 4 5 6 7 8 9
+c4  0 1 2 3 4 5 6 7 8 9
+c5  0 1 2 3 4 5 6 7 8 9
+c6  0 1 2 3 4 5 6 7 8 9
+c7  0 1 2 3 4 5 6 7 8 9
+c8  0 1 2 3 4 5 6 7 8 9
+c9  0 1 2 3 4 5 6 7 8 9
+c10 0 1 2 3 4 5 6 7 8 9
+
+testCheck()
+exit
